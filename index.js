39e1ae930bb0511d44d7fce58458946946eda95e
marknosal <118954734+marknosal@users.noreply.github.com>
2023-01-24 23:42:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 // Write your code here!
-document.querySelector('main#main').remove();
-const newHeader = document.createElement('h1');
-newHeader.id = "victory";
-newHeader.textContent = "Mark is the champion";
